42163e719039898fb2dea1ed7ca4b64f82f90c09
5vermind <39900428+5vermind@users.noreply.github.com>
2018-07-23 18:00:55 +0900

    Update UserService.java

--- src/main/java/com/example/webservice/web/UserService.java
+++ src/main/java/com/example/webservice/web/UserService.java
@@ -1,10 +1,10 @@
 package com.example.webservice.web;
 
 
-import com.example.webservice.data.Users;
-import com.example.webservice.data.UsersRepository;
+import com.example.webservice.data.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import static com.example.webservice.data.Hashash.hundredTime;
 
 
 @Service
@@ -12,36 +12,43 @@ public class UserService {
     @Autowired
     UsersRepository usersRepository;
 
-
-    public String IogIn(String id, String pw){ //로그인 메소드
+    public String logIn(String id, String pw){ //로그인 메소드
        if (usersRepository.existsById(id) == true){
-           if (usersRepository.findById(id).get().getPassword().equals(pw)){
-               return "1"; }
+           String digest = hundredTime(usersRepository.findById(id).get().getSalt(), pw);
+           if(digest.equals(usersRepository.findById(id).get().getDigest())){
+               return "1";
+           }
            else { return "0"; }
        }
        else { return "0"; }
     }
-    public String signUp(String id, String pw){ //사인업 메소드
+    public String signUp(String id, String pw) { //사인업 메소드
         if (usersRepository.existsById(id) == false){
+            String salt = RandomString.randomString(32);
+            String digest = hundredTime(salt, pw);
             Users users = new Users();
             users.setId(id);
-            users.setPassword(pw);
+            users.setSalt(salt);
+            users.setDigest(digest);
             usersRepository.save(users);
             return "1";
         }
         else{ return "0"; }
-    }
 
+    }
     public String changePw(String id, String cur_pw, String new_pw){
-        if (usersRepository.existsById(id) == true && usersRepository.findById(id).get().getPassword().equals(cur_pw)){
-            usersRepository.findById(id).get().setPassword(new_pw);
+        if (logIn(id, cur_pw) == "1"){infodb
+            String salt = RandomString.randomString(32);
+            String digest = hundredTime(salt, new_pw);
+            usersRepository.findById(id).get().setSalt(salt);
+            usersRepository.findById(id).get().setDigest(digest);
             usersRepository.save(usersRepository.findById(id).get());
             return "1";
         }
         else { return "0"; }
     }
     public String cancelId(String id, String pw){
-        if(usersRepository.existsById(id) && usersRepository.findById(id).get().getPassword().equals(pw)){
+        if(logIn(id, pw) == "1"){
             usersRepository.delete(usersRepository.findById(id).get());
             return "1";
         }
