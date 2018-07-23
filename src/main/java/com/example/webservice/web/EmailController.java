83eae4492ca93cfd17fe17c6d6735fc620b7d45d
5vermind <39900428+5vermind@users.noreply.github.com>
2018-07-23 18:00:27 +0900

    Update EmailController.java

--- src/main/java/com/example/webservice/web/EmailController.java
+++ src/main/java/com/example/webservice/web/EmailController.java
@@ -1,14 +1,12 @@
 package com.example.webservice.web;
 
-import com.example.webservice.data.Email;
-import com.example.webservice.data.EmailSender;
-import com.example.webservice.data.Users;
-import com.example.webservice.data.UsersRepository;
+import com.example.webservice.data.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -17,54 +15,45 @@ import java.util.Random;
 import  java.sql.*;
 
 
-@Controller
+@Service
 public class EmailController {
 	@Autowired
 	private EmailSender emailSender;
 	@Autowired
 	UsersRepository usersRepository;
-    private static Connection con;
-    private  static Statement stmt;
 
-	@RequestMapping("/send")
-	public String sendEmailAction(HttpServletRequest request) throws Exception{
+	public String sendEmailAction(String id, String what) throws Exception{
 		Email email = new Email();
-		String receiver = request.getParameter("email");
-		String subject = "임시 비밀번호 for withus";
-		if (usersRepository.existsById(receiver) == false){
-		    return "0";
+		String subject="0";
+		String content="0";
+		if (what.equals("signup")) {
+            subject = "이메일 본인인증 for withus";
+            if (usersRepository.existsById(id) == true){
+                return "0";
+            }
         }
+        else if (what.equals("changepw")){
+		    subject = "임시 비밀번호 for withus";
+            if (usersRepository.existsById(id) == false){
+                return "0";
+            }
+        }
+		String temp = RandomString.randomString(6);
 
-		StringBuffer temp = new StringBuffer();
-		Random rnd = new Random();
-		for (int i = 0; i < 6; i++) {
-			int rIndex = rnd.nextInt(3);
-			switch (rIndex) {
-				case 0:
-					// a-z
-					temp.append((char) ((int) (rnd.nextInt(26)) + 97));
-					break;
-				case 1:
-					// A-Z
-					temp.append((char) ((int) (rnd.nextInt(26)) + 65));
-					break;
-				case 2:
-					// 0-9
-					temp.append((rnd.nextInt(10)));
-					break;
-			}
-		}
-
-		String content = "회원님의 임시 비밀번호는 " + temp.toString() + "입니다.";
-
-        usersRepository.findById(receiver).get().setPassword(temp.toString());
-		usersRepository.save(usersRepository.findById(receiver).get());
-
-		email.setReceiver (receiver);
-		email.setSubject(subject);
-		email.setContent(content);
-		emailSender.SendEmail(email);
-		
-		return "1";
+        if (what.equals("signup")) {
+            content = "인증창에" + temp + "를 입력하세요";
+        }
+        else if (what.equals("changepw")){
+		    content ="회원님의 임시 비밀번호는 " + temp + "입니다.";
+		    String salt = RandomString.randomString(32);
+            usersRepository.findById(id).get().setSalt(salt);
+            usersRepository.findById(id).get().setSalt(Hashash.hundredTime(salt, temp.toString()));
+            usersRepository.save(usersRepository.findById(id).get());
+        }
+        email.setReceiver (id);
+        email.setSubject(subject);
+        email.setContent(content);
+        emailSender.SendEmail(email);
+        return temp.toString();
 	}
 }
