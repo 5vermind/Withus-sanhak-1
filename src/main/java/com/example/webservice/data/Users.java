15152a6605061311862e0ad987e95a92097b4953
5vermind <39900428+5vermind@users.noreply.github.com>
2018-07-23 17:54:19 +0900

    Update Users.java

--- src/main/java/com/example/webservice/data/Users.java
+++ src/main/java/com/example/webservice/data/Users.java
@@ -8,23 +8,32 @@ public class Users {
 	@Id
 	@Column(columnDefinition = "VARCHAR(50)")
 	String id;
-	@Column(columnDefinition = "VARCHAR(50)")
-	String password;
-
+	@Column(columnDefinition = "VARCHAR(32)")
+	String salt;
+	@Column(columnDefinition = "VARCHAR(200)")
+	String digest;
 
 	public String getId() {
 		return id;
 	}
 
-	public String getPassword() {
-		return password;
+	public String getSalt() {
+		return salt;
 	}
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
 
-	public void setId(String id) {
+    public void setId(String id) {
 		this.id = id;
 	}
+
+    public String getDigest() {
+        return digest;
+    }
+
+    public void setDigest(String digest) {
+        this.digest = digest;
+    }
 }
