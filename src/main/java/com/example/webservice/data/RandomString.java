eb8420e2f83d7fe563d94a80e83f1dde42732f2c
5vermind <39900428+5vermind@users.noreply.github.com>
2018-07-23 17:55:18 +0900

    Create RandomString.java

---
+++ src/main/java/com/example/webservice/data/RandomString.java
@@ -0,0 +1,28 @@
+package com.example.webservice.data;
+
+import java.util.Random;
+
+public class RandomString {
+    public static String randomString(int num){
+        StringBuffer temp = new StringBuffer();
+        Random rnd = new Random();
+        for (int i = 0; i < num; i++) {
+            int rIndex = rnd.nextInt(3);
+            switch (rIndex) {
+                case 0:
+                    // a-z
+                    temp.append((char) ((int) (rnd.nextInt(26)) + 97));
+                    break;
+                case 1:
+                    // A-Z
+                    temp.append((char) ((int) (rnd.nextInt(26)) + 65));
+                    break;
+                case 2:
+                    // 0-9
+                    temp.append((rnd.nextInt(10)));
+                    break;
+            }
+        }
+        return temp.toString();
+    }
+}
